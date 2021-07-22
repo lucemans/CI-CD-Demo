ae84105e5f3a261b612881ca856c5cfc36fd464c
Luc <luc@lucemans.nl>
2021-07-22 10:06:10 +0200

    Initial Commit :wave:

---
+++ src/index.test.ts
@@ -0,0 +1,20 @@
+import {add} from './';
+
+it("should add 5 and 6 together", () => {
+    expect(add(5,6)).toEqual(11);
+});
+it("should add 1 and 2 together", () => {
+    expect(add(1,2)).toEqual(3);
+});
+it("should add -5 and -6 together", () => {
+    expect(add(-5,-6)).toEqual(-11);
+});
+it("should add 1 and 0 together", () => {
+    expect(add(1,0)).toEqual(1);
+});
+it("should add 1 and null together", () => {
+    expect(add(1,null)).toEqual(1);
+});
+it("should add 5 and 6 together", () => {
+    expect(add(null,null)).toEqual(0);
+});
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+
+
+
+
+
+
+// This file is going to very much overcomplicate addition xD
+export const add = (...a: number[]) => a.reduce((a,b)=>a+b,0);
